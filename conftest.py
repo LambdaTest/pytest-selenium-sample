c736995c6dcd50513b724973eef2c8c0880b18e5
Saurabh-LT <saurabh@lambdatest.com>
2022-07-12 15:52:24 +0530

    fix for the Pytest

--- conftest.py
+++ conftest.py
@@ -1,20 +1,18 @@
-import pytest
 from os import environ
 
+import pytest
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 
 
 @pytest.fixture(scope='function')
 def driver(request):
-
     desired_caps = {}
 
     browser = {
         "platform": "Windows 10",
         "browserName": "chrome",
-        "version": "73"
+        "version": "latest"
     }
 
     desired_caps.update(browser)
@@ -27,28 +25,30 @@ def driver(request):
     selenium_endpoint = "http://{}:{}@hub.lambdatest.com/wd/hub".format(username, access_key)
     desired_caps['build'] = build
     desired_caps['name'] = test_name
-    desired_caps['video']= True
-    desired_caps['visual']= True
-    desired_caps['network']= True
-    desired_caps['console']= True
+    desired_caps['video'] = True
+    desired_caps['visual'] = True
+    desired_caps['network'] = True
+    desired_caps['console'] = True
+    caps = {"LT:Options": desired_caps}
 
-    executor = RemoteConnection(selenium_endpoint, resolve_ip=False)
+    executor = RemoteConnection(selenium_endpoint)
     browser = webdriver.Remote(
         command_executor=executor,
-        desired_capabilities=desired_caps
+        desired_capabilities=caps
     )
     yield browser
 
-
-
     def fin():
-        #browser.execute_script("lambda-status=".format(str(not request.node.rep_call.failed if "passed" else "failed").lower()))
+        # browser.execute_script("lambda-status=".format(str(not request.node.rep_call.failed if "passed" else
+        # "failed").lower()))
         if request.node.rep_call.failed:
             browser.execute_script("lambda-status=failed")
         else:
             browser.execute_script("lambda-status=passed")
             browser.quit()
+
     request.addfinalizer(fin)
+
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
@@ -57,6 +57,6 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     rep = outcome.get_result()
 
-    # set an report attribute for each phase of a call, which can
+    # set a report attribute for each phase of a call, which can
     # be "setup", "call", "teardown"
     setattr(item, "rep_" + rep.when, rep)
