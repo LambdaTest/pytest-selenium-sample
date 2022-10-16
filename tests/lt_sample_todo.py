fbdb6a16aaf474a143385171c3592444527aa1ef
ajayayyappanavar <101085520+ajayayyappanavar@users.noreply.github.com>
2022-10-16 18:47:26 +0530

    Update lt_sample_todo.py

--- tests/lt_sample_todo.py
+++ tests/lt_sample_todo.py
@@ -1,36 +1,39 @@
 import pytest
 from selenium.webdriver.common.by import By
 import sys
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 
 @pytest.mark.usefixtures('driver')
 class TestLink:
 
     def test_title(self, driver):
-        """
-        Verify click and title of page
-        :return: None
-        """
-        driver.get('https://lambdatest.github.io/sample-todo-app/')
+        
+        driver.get(r'https://www.lambdatest.com/selenium-playground/')
         driver.implicitly_wait(10)
-        driver.find_element(By.NAME, "li1").click()
-        driver.find_element(By.NAME, "li2").click()
-
-        title = "Sample page - lambdatest.com"
-        assert title == driver.title
+        driver.find_element(By.XPATH, "//a[contains(text(),'Simple Form Demo')]").click()
+        title = driver.driver.current_url
+        assert “simple-form-demo” in title, "tetx is not present n the URL"
+        val = “Welcome to LambdaTest”
+        driver.find_element(By.XPATH, "//input[@id='user-message']").send_keys(val)
+        driver.find_element(By.ID, "#showInput").click()
+        text = driver.find_element(By.CSS_SELECTOR, '#message').text
+        assert text == val
+       
 
     def test_item(self, driver):
         """
         Verify item submission
         :return: None
         """
-        driver.get('https://lambdatest.github.io/sample-todo-app/')
-        sample_text = "Happy Testing at LambdaTest"
-        email_text_field = driver.find_element(By.ID, "sampletodotext")
-        email_text_field.send_keys(sample_text)
+        
+        driver.get('https://www.lambdatest.com/selenium-playground/')
+        driver.find_element(By.XPATH,"//a[contains(text(),'Drag & Drop Sliders')]").click()
+        time.sleep(2)
+        ActionChains(driver).move_by_offset(946, 349).pause(2).click().perform()
+        exp_val= '95'
+        obtained_val = driver.find_element(By.ID,"rangeSuccess").text
+        assertEqual(exp_val, obtained_val, "Value is not equal to 95")
 
-        driver.find_element(By.ID, "addbutton").click()
-
-        li6 = driver.find_element(By.NAME, "li6")
-        # sys.stderr.write(li6)
-        # assert sample_text in li6
+      
